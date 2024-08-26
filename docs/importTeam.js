53da250c4fd9c284b8c3a5ec9101a68664376077
Damian <damiansuaz@gmail.com>
2024-08-26 09:04:46 -0400

    Bring in getJSON function from index

--- docs/importTeam.js
+++ docs/importTeam.js
@@ -1,10 +1,44 @@
-const insEle = document.querySelector("#inserthere");
-fetch('pkmnteams.json')
+const debugLine = document.querySelector("#inserthere");
+
+const JSONfile = 'pkmnteams.json';
+const getJSON = function(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        callback(null, xhr.response);
+	return;
+      } 
+	callback(status, xhr.response);
+    };
+    xhr.send();
+};
+
+getJSON(JSONfile, function(err, data) {
+	if (err !== null) {
+		alert("Hmm..." + err);
+		const importTeam = {};
+		return;
+	}
+	const importTeam = data;
+	for (const gen in filterList.children){
+		console.log(gen.innerHTML);
+	};
+	for (const gen in importTeam) {
+		console.log(`${gen}: ${importTeam[gen].length}`);
+		for (const poke in importTeam[gen]){
+  			console.log(`${importTeam[gen][poke].name}`);
+  		};
+	};
+});
+
+fetch(JSONfile)
   .then(res => {
     return res.json();
   })
   .then(data => {
     console.log(data);
-		insEle.innerHTML = "Data Imported.";
+		debugLine.innerHTML = "Data Imported.";
   })
   .catch(err => console("Hmm, this problem happened...: " + err));
